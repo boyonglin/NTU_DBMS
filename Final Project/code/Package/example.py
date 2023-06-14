113222baed780649981279a8a8c328283cd9323b
Clancy Lin <poerevan9@gmail.com>
2023-06-14 16:37:27 +0800

    Integrate excel GUI

--- Final Project/code/Package/example.py
+++ Final Project/code/Package/example.py
@@ -1,18 +1,19 @@
 import xlwings as xw
 import pandas as pd
 import PCA, kNN
+import sys
 
 # Testing arguments
-arg1 = (1, 1)
-arg2 = (1, 14)
-arg3 = (2, 1)
-arg4 = (31, 14)
+arg1 = int(sys.argv[1])  #(10, 1)
+arg2 = int(sys.argv[2])  #(10, 14)
+arg3 = int(sys.argv[3])  #(11, 1)
+arg4 = int(sys.argv[4])  #(40, 14)
 
 # Capture the data
 wb = xw.apps.active.books.active
-ws = wb.sheets['main_sheet']
-df = ws.range(arg3, arg4).options(pd.DataFrame, index=False, header=False).value
-header = ws.range(arg1, arg2).value
+ws = wb.sheets['main-sheet']
+df = ws.range((arg1+1, arg2), (arg3, arg4)).options(pd.DataFrame, index=False, header=False).value
+header = ws.range((arg1, arg2),(arg1, arg4)).value
 df.columns = header
 df = df.reset_index(drop=True)
 
@@ -28,6 +29,6 @@ sheet_names = [sheet.name for sheet in wb.sheets]
 if 'wine-test' in sheet_names:
     sheet = wb.sheets['wine-test']
 else:
-    sheet = wb.sheets.add(name='wine-test', before=None, after=None)
+    sheet = wb.sheets.add(name='wine-test', before=None, after='main-sheet')
 
 sheet.range('A1').options(expand='table').value = df_analysis
